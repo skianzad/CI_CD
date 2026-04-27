4ca93756839ef464b8f57e86c8916db5042ea76f
Soheil Kianzad <soheil@senscilab.com>
2026-04-27 15:25:31 -0400

    Update the app to use flask

--- app.py
+++ app.py
@@ -5,7 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 
 def home():
-    return "CI/CD is running"
+    raise Exception("Internal error just for testing")
+
+@app.route("/health")
+def health():
+    return "OK"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port = 5000)
